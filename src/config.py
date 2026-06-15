2a0ef9f5e9cec5312e0f49f79670d4441faa5de4
Pocito80 <leckimaciek2000@gmail.com>
2026-06-15 20:06:00 +0200

    refactor - config init

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 class ThresholdConfig:
-    def __init__(self, warningLevel, criticalLevel):
-        self.warningLevel = warningLevel
-        self.criticalLevel = criticalLevel
+    def __init__(self, warningLevel: float, criticalLevel: float) -> None:
+        self.warningLevel: float = warningLevel
+        self.criticalLevel: float = criticalLevel
