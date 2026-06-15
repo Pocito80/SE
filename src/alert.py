205b21971c62d6a94d0412887a1fdb5ddf77920c
Pocito80 <leckimaciek2000@gmail.com>
2026-06-15 20:17:22 +0200

    green - email sending

--- src/alert.py
+++ src/alert.py
@@ -1,3 +1,9 @@
+def send_email_api(operatorID: str, message: str) -> None:
+    pass
+
 class AlertManager:
     def triggerVisualAlert(self, level: float) -> None:
         pass
+
+    def dispatchEmail(self, operatorID: str) -> None:
+        send_email_api(operatorID=operatorID, message="Critical water level alert!")
