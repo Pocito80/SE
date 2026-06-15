c7c98a2b5f9809ea9a6248292b6c0ab95dc4dcb9
Pocito80 <leckimaciek2000@gmail.com>
2026-06-15 20:07:47 +0200

    green - threshold update

--- src/config.py
+++ src/config.py
@@ -2,3 +2,7 @@ class ThresholdConfig:
     def __init__(self, warningLevel: float, criticalLevel: float) -> None:
         self.warningLevel: float = warningLevel
         self.criticalLevel: float = criticalLevel
+
+    def updateThresholds(self, warningLevel, criticalLevel):
+        self.warningLevel = warningLevel
+        self.criticalLevel = criticalLevel
